f03f6ae482b3a41cf52cbd5b8a854172d620c979
Capovillaa <160801041+Capovillaa@users.noreply.github.com>
2024-05-13 09:13:58 -0300

    Atualizar o Fase2.py

--- Fase2.py
+++ Fase2.py
@@ -1,9 +1,9 @@
 import oracledb
 try:
  conexao = oracledb.connect(
- user="system",
- password="capo1950",
- dsn="localhost/XEPDB1")
+ user="BD150224114",
+ password="Mwabf7",
+ dsn="BD-ACD/xe ")
 except Exception as erro:
  print ('Erro em conexão', erro)
 else:
@@ -22,66 +22,68 @@ impostos=[]
 margem_lucro=[]
 
 cont=0
-
-for i in range(len(estoque)+1):
+indice = 0
     
-    cursor.execute("select id_prod from estoque")
-    id_prod = [row[0] for row in cursor.fetchall()]
+cursor.execute("select id_prod from estoque")
+id_prod = [row[0] for row in cursor.fetchall()]
 
-    cursor.execute("select nome_prod from estoque")
-    nome_prod = [row[0] for row in cursor.fetchall()]
+cursor.execute("select nome_prod from estoque")
+nome_prod = [row[0] for row in cursor.fetchall()]
 
-    cursor.execute("select desc_prod from estoque")
-    desc_prod = [row[0] for row in cursor.fetchall()]
+cursor.execute("select desc_prod from estoque")
+desc_prod = [row[0] for row in cursor.fetchall()]
 
-    cursor.execute("select custo_prod from estoque")
-    custo_prod = [row[0] for row in cursor.fetchall()]
+cursor.execute("select custo_prod from estoque")
+custo_prod = [row[0] for row in cursor.fetchall()]
 
-    cursor.execute("select custo_fixo from estoque")
-    custo_fixo = [row[0] for row in cursor.fetchall()]
+cursor.execute("select custo_fixo from estoque")
+custo_fixo = [row[0] for row in cursor.fetchall()]
 
-    cursor.execute("select comissao_vendas from estoque")
-    comissao_vendas = [row[0] for row in cursor.fetchall()]
+cursor.execute("select comissao_vendas from estoque")
+comissao_vendas = [row[0] for row in cursor.fetchall()]
 
-    cursor.execute("select impostos from estoque")
-    impostos = [row[0] for row in cursor.fetchall()]
+cursor.execute("select impostos from estoque")
+impostos = [row[0] for row in cursor.fetchall()]
 
-    cursor.execute("select margem_lucro from estoque")
-    margem_lucro = [row[0] for row in cursor.fetchall()]
+cursor.execute("select margem_lucro from estoque")
+margem_lucro = [row[0] for row in cursor.fetchall()]
 
-    estoque.append(id_prod)
-    estoque.append(nome_prod)
-    estoque.append(desc_prod)
-    estoque.append(custo_prod)
-    estoque.append(custo_fixo)
-    estoque.append(comissao_vendas)
-    estoque.append(impostos)
-    estoque.append(margem_lucro)
+estoque.append(id_prod)
+estoque.append(nome_prod)
+estoque.append(desc_prod)
+estoque.append(custo_prod)
+estoque.append(custo_fixo)
+estoque.append(comissao_vendas)
+estoque.append(impostos)
+estoque.append(margem_lucro)
 
-    PV=custo_prod[0] / (1 - ((custo_fixo[0] + comissao_vendas[0] + impostos[0] + margem_lucro[0])/100))
+cursor.close
+conexao.close
 
-    print (f"Produto: {nome_prod[0]}   Especifcacão: {desc_prod[0]}")
+
+for i in (estoque):
+    PV=custo_prod[indice] / (1 - ((custo_fixo[indice] + comissao_vendas[indice] + impostos[indice] + margem_lucro[indice])/100))
+
+    print (f"Produto: {nome_prod[indice]}   Especifcacão: {desc_prod[indice]}")
     print ("---------------------------------------------------------")
     print (f"Descriçao:                            Valor       %")
     print (f"A. Preço de Venda                  =  R${PV:.2f}     100.0%")
-    print (f"B. Custo de Aquisição (Fornecedor) =  R${custo_prod[0]:.2f}     {(100*custo_prod[0]/PV):.2f}%") 
-    print (f"C. Receita Bruta                   =  R${(PV-custo_prod[0]):.2f}     {(((PV-custo_prod[0])*100)/PV):.2f}%")
-    print (f"D. Custo Fixo/Administrativo       =  R${((custo_fixo[0]*PV)/100):.2f}     {custo_fixo[0]:.2f}%")
-    print (f"E. Comissão de Vendas              =  R${((comissao_vendas[0]*PV)/100):.2f}      {comissao_vendas[0]:.2f}%")
-    print (f"F. Impostos                        =  R${((impostos[0]*PV)/100):.2f}      {impostos[0]:.2f}%")
-    print (f"G. Outros Custos (D+E+F)           =  R${(PV*(custo_fixo[0]/100)+PV*(comissao_vendas[0]/100)+PV*(impostos[0]/100)):.2f}     {impostos[0]+comissao_vendas[0]+custo_fixo[0]:.2f}%")
-    print (f"H. Rentabilidade (C-G)             =  R${((margem_lucro[0]*PV)/100):.2f}     {margem_lucro[0]:.2f}%")
+    print (f"B. Custo de Aquisição (Fornecedor) =  R${custo_prod[indice]:.2f}     {(100*custo_prod[indice]/PV):.2f}%") 
+    print (f"C. Receita Bruta                   =  R${(PV-custo_prod[indice]):.2f}     {(((PV-custo_prod[indice])*100)/PV):.2f}%")
+    print (f"D. Custo Fixo/Administrativo       =  R${((custo_fixo[indice]*PV)/100):.2f}     {custo_fixo[indice]:.2f}%")
+    print (f"E. Comissão de Vendas              =  R${((comissao_vendas[indice]*PV)/100):.2f}      {comissao_vendas[indice]:.2f}%")
+    print (f"F. Impostos                        =  R${((impostos[indice]*PV)/100):.2f}      {impostos[indice]:.2f}%")
+    print (f"G. Outros Custos (D+E+F)           =  R${(PV*(custo_fixo[indice]/100)+PV*(comissao_vendas[indice]/100)+PV*(impostos[indice]/100)):.2f}     {impostos[indice]+comissao_vendas[indice]+custo_fixo[indice]:.2f}%")
+    print (f"H. Rentabilidade (C-G)             =  R${((margem_lucro[indice]*PV)/100):.2f}     {margem_lucro[indice]:.2f}%")
 
-    if margem_lucro[0] > 20:
+    if margem_lucro[indice] > 20:
         print("\nO Lucro Deste Produto é Alto.\n")
-    elif margem_lucro[0] > 10 and margem_lucro[0] <= 20:
+    elif margem_lucro[indice] > 10 and margem_lucro[indice] <= 20:
         print("\nO Lucro Deste Produto é Médio.\n")
-    elif margem_lucro[0] > 0 and margem_lucro[0] <= 10:
+    elif margem_lucro[indice] > 0 and margem_lucro[indice] <= 10:
         print("\nO Lucro Deste Produto é Baixo.\n")
-    elif margem_lucro[0] == 0:
+    elif margem_lucro[indice] == 0:
         print("\nEste Produto Não Gera Lucro ou Pejuízo (Equilíbrio).\n")
-    elif margem_lucro[0] < 0:
+    elif margem_lucro[indice] < 0:
         print("\nEste Produto Gera Prejuízo.\n")
-    
-cursor.close
-conexao.close
+    indice+=1
