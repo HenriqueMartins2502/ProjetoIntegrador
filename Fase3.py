cd3f248cbc3dee478db553f6067130d7d066fada
Mike Ensergueix <161461121+M1K32005@users.noreply.github.com>
2024-06-02 23:40:45 -0300

    Update Fase3.py

--- Fase3.py
+++ Fase3.py
@@ -259,3 +259,256 @@ while (num != 0):
         conexao.commit()
 cursor.close
 conexao.close
+
+    if num == 4:
+        print('-'*50)
+        print("1. Listar Todos os Produtos")
+        print("2. Selecionar Produto por ID")
+        print("0. Voltar")
+
+        num_2 = int(input("Digite para Selecionar:"))
+
+        if (num_2 == 1):
+            #definição das listas
+            estoque=[]
+            id_prod=[]
+            nome_prod=[]
+            desc_prod=[]
+            custo_prod=[]
+            custo_fixo=[]
+            comissao_vendas=[]
+            impostos=[]
+            margem_lucro=[]
+                
+            cursor.execute("select id_prod from estoque")
+            id_prod = [row[0] for row in cursor.fetchall()]
+
+            cursor.execute("select nome_prod from estoque")
+            nome_prod = [row[0] for row in cursor.fetchall()]
+
+            cursor.execute("select desc_prod from estoque")
+            desc_prod = [row[0] for row in cursor.fetchall()]
+
+            cursor.execute("select custo_prod from estoque")
+            custo_prod = [row[0] for row in cursor.fetchall()]
+
+            cursor.execute("select custo_fixo from estoque")
+            custo_fixo = [row[0] for row in cursor.fetchall()]
+
+            cursor.execute("select comissao_vendas from estoque")
+            comissao_vendas = [row[0] for row in cursor.fetchall()]
+
+            cursor.execute("select impostos from estoque")
+            impostos = [row[0] for row in cursor.fetchall()]
+
+            cursor.execute("select margem_lucro from estoque")
+            margem_lucro = [row[0] for row in cursor.fetchall()]
+
+            estoque.append(id_prod)
+            estoque.append(nome_prod)
+            estoque.append(desc_prod)
+            estoque.append(custo_prod)
+            estoque.append(custo_fixo)
+            estoque.append(comissao_vendas)
+            estoque.append(impostos)
+            estoque.append(margem_lucro)
+
+            cursor.close
+            conexao.close
+
+            import numpy as np
+
+            alfabeto = "abcdefghijklmnopqrstuvwxyz"
+
+            chave = "test"
+
+            letra_to_num = dict(zip(alfabeto, range(len(alfabeto))))
+            num_to_letra = dict(zip(range(len(alfabeto)), alfabeto))
+
+            chave_num = [letra_to_num[i] for i in chave]
+
+            chave_matriz = np.array(chave_num).reshape(2, 2)
+
+            matriz_deci = np.linalg.inv(chave_matriz) * round(np.linalg.det(chave_matriz))
+
+            chave_inv = (9 * matriz_deci) % 26
+
+            for i in range (len(id_prod)):
+                Desc_cod = desc_prod[i]
+
+                Desc_num = [letra_to_num[i] for i in Desc_cod]
+
+                Desc_array = np.array(Desc_num)
+
+                Desc_pares = np.split(Desc_array, len(Desc_num)//2)
+
+                Desc_matriz = [np.matmul(Desc_pares[i], chave_inv) % 26 for i in range(len(Desc_pares))]
+
+                Desc_array = np.concatenate(Desc_matriz)
+
+                Desc_deci = [num_to_letra[round(Desc_array[i])] for i in range(len(Desc_array))]
+
+                Desc_deci = ''.join(Desc_deci)
+
+                PV=custo_prod[i] / (1 - ((custo_fixo[i] + comissao_vendas[i] + impostos[i] + margem_lucro[i])/100))
+
+                print (f"Produto: {nome_prod[i]}   Especifcacão: {Desc_deci}")
+                print ("---------------------------------------------------------")
+                print (f"Descriçao:                            Valor       %")
+                print (f"A. Preço de Venda                  =  R${PV:.2f}     100.0%")
+                print (f"B. Custo de Aquisição (Fornecedor) =  R${custo_prod[i]:.2f}     {(100*custo_prod[i]/PV):.2f}%") 
+                print (f"C. Receita Bruta                   =  R${(PV-custo_prod[i]):.2f}     {(((PV-custo_prod[i])*100)/PV):.2f}%")
+                print (f"D. Custo Fixo/Administrativo       =  R${((custo_fixo[i]*PV)/100):.2f}     {custo_fixo[i]:.2f}%")
+                print (f"E. Comissão de Vendas              =  R${((comissao_vendas[i]*PV)/100):.2f}      {comissao_vendas[i]:.2f}%")
+                print (f"F. Impostos                        =  R${((impostos[i]*PV)/100):.2f}      {impostos[i]:.2f}%")
+                print (f"G. Outros Custos (D+E+F)           =  R${(PV*(custo_fixo[i]/100)+PV*(comissao_vendas[i]/100)+PV*(impostos[i]/100)):.2f}     {impostos[i]+comissao_vendas[i]+custo_fixo[i]:.2f}%")
+                print (f"H. Rentabilidade (C-G)             =  R${((margem_lucro[i]*PV)/100):.2f}     {margem_lucro[i]:.2f}%")
+
+                if margem_lucro[i] > 20:
+                    print("\nO Lucro Deste Produto é Alto.\n")
+                elif margem_lucro[i] > 10 and margem_lucro[i] <= 20:
+                    print("\nO Lucro Deste Produto é Médio.\n")
+                elif margem_lucro[i] > 0 and margem_lucro[i] <= 10:
+                    print("\nO Lucro Deste Produto é Baixo.\n")
+                elif margem_lucro[i] == 0:
+                    print("\nEste Produto Não Gera Lucro ou Pejuízo (Equilíbrio).\n")
+                elif margem_lucro[i] < 0:
+                    print("\nEste Produto Gera Prejuízo.\n")
+            cursor.close
+            conexao.close
+
+        while (num_2 == 2):
+
+            ID = int(input("Digite o ID do Produto:"))
+            #definição das listas
+            estoque=[]
+            id_prod=[]
+            nome_prod=[]
+            desc_prod=[]
+            custo_prod=[]
+            custo_fixo=[]
+            comissao_vendas=[]
+            impostos=[]
+            margem_lucro=[]
+                
+            cursor.execute("""select id_prod from estoque
+                           where id_prod = {ID}""")
+            id_prod = [row[0] for row in cursor.fetchall()]
+
+            cursor.execute("""select nome_prod from estoque
+                           where id_prod = {ID}""")
+            nome_prod = [row[0] for row in cursor.fetchall()]
+
+            cursor.execute("""select desc_prod from estoque
+                           where id_prod = {ID}""")
+            desc_prod = [row[0] for row in cursor.fetchall()]
+
+            cursor.execute("""select custo_prod from estoque
+                           where id_prod = {ID}""")
+            custo_prod = [row[0] for row in cursor.fetchall()]
+
+            cursor.execute("""select custo_fixo from estoque
+                           where id_prod = {ID}""")
+            custo_fixo = [row[0] for row in cursor.fetchall()]
+
+            cursor.execute("""select comissao_vendas from estoque
+                           where id_prod = {ID}""")
+            comissao_vendas = [row[0] for row in cursor.fetchall()]
+
+            cursor.execute("""select impostos from estoque
+                           where id_prod = {ID}""")
+            impostos = [row[0] for row in cursor.fetchall()]
+
+            cursor.execute(""""select margem_lucro from estoque
+                           where id_prod = {ID}""")
+            margem_lucro = [row[0] for row in cursor.fetchall()]
+
+            estoque.append(id_prod)
+            estoque.append(nome_prod)
+            estoque.append(desc_prod)
+            estoque.append(custo_prod)
+            estoque.append(custo_fixo)
+            estoque.append(comissao_vendas)
+            estoque.append(impostos)
+            estoque.append(margem_lucro)
+
+            cursor.close
+            conexao.close
+
+            import numpy as np
+
+            alfabeto = "abcdefghijklmnopqrstuvwxyz"
+
+            chave = "test"
+
+            letra_to_num = dict(zip(alfabeto, range(len(alfabeto))))
+            num_to_letra = dict(zip(range(len(alfabeto)), alfabeto))
+
+            chave_num = [letra_to_num[i] for i in chave]
+
+            chave_matriz = np.array(chave_num).reshape(2, 2)
+
+            matriz_deci = np.linalg.inv(chave_matriz) * round(np.linalg.det(chave_matriz))
+
+            chave_inv = (9 * matriz_deci) % 26
+
+            for i in range (len(id_prod)):
+                Desc_cod = desc_prod[i]
+
+                Desc_num = [letra_to_num[i] for i in Desc_cod]
+
+                Desc_array = np.array(Desc_num)
+
+                Desc_pares = np.split(Desc_array, len(Desc_num)//2)
+
+                Desc_matriz = [np.matmul(Desc_pares[i], chave_inv) % 26 for i in range(len(Desc_pares))]
+
+                Desc_array = np.concatenate(Desc_matriz)
+
+                Desc_deci = [num_to_letra[round(Desc_array[i])] for i in range(len(Desc_array))]
+
+                Desc_deci = ''.join(Desc_deci)
+
+                PV=custo_prod[i] / (1 - ((custo_fixo[i] + comissao_vendas[i] + impostos[i] + margem_lucro[i])/100))
+
+                print (f"Produto: {nome_prod[i]}   Especifcacão: {Desc_deci}")
+                print ("---------------------------------------------------------")
+                print (f"Descriçao:                            Valor       %")
+                print (f"A. Preço de Venda                  =  R${PV:.2f}     100.0%")
+                print (f"B. Custo de Aquisição (Fornecedor) =  R${custo_prod[i]:.2f}     {(100*custo_prod[i]/PV):.2f}%") 
+                print (f"C. Receita Bruta                   =  R${(PV-custo_prod[i]):.2f}     {(((PV-custo_prod[i])*100)/PV):.2f}%")
+                print (f"D. Custo Fixo/Administrativo       =  R${((custo_fixo[i]*PV)/100):.2f}     {custo_fixo[i]:.2f}%")
+                print (f"E. Comissão de Vendas              =  R${((comissao_vendas[i]*PV)/100):.2f}      {comissao_vendas[i]:.2f}%")
+                print (f"F. Impostos                        =  R${((impostos[i]*PV)/100):.2f}      {impostos[i]:.2f}%")
+                print (f"G. Outros Custos (D+E+F)           =  R${(PV*(custo_fixo[i]/100)+PV*(comissao_vendas[i]/100)+PV*(impostos[i]/100)):.2f}     {impostos[i]+comissao_vendas[i]+custo_fixo[i]:.2f}%")
+                print (f"H. Rentabilidade (C-G)             =  R${((margem_lucro[i]*PV)/100):.2f}     {margem_lucro[i]:.2f}%")
+
+                if margem_lucro[i] > 20:
+                    print("\nO Lucro Deste Produto é Alto.\n")
+                elif margem_lucro[i] > 10 and margem_lucro[i] <= 20:
+                    print("\nO Lucro Deste Produto é Médio.\n")
+                elif margem_lucro[i] > 0 and margem_lucro[i] <= 10:
+                    print("\nO Lucro Deste Produto é Baixo.\n")
+                elif margem_lucro[i] == 0:
+                    print("\nEste Produto Não Gera Lucro ou Pejuízo (Equilíbrio).\n")
+                elif margem_lucro[i] < 0:
+                    print("\nEste Produto Gera Prejuízo.\n")
+            cursor.close
+            conexao.close
+
+            print('-'*50)
+            print("1. Listar Todos os Produtos")
+            print("2. Selecionar Produto por ID")
+            print("0. Voltar")
+
+            num_2 = int(input("Digite para Selecionar:"))
+
+        if (num_2 == 0):
+            continue
+
+elif (num == 0):
+    print ('-'*50)
+    print ("Programa Finalizado.")\
+
+else:
+    print ("Opcão Inválida.")
